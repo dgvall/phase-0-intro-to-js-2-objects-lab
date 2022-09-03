e79efacf1037b3e509f507e4b2df51be064097e1
Diego Valles <dgvall134@gmail.com>
2022-09-03 15:38:33 -0600

    commit tree 39bdc7beddddcf74d466abf5e8828d7f7ae2ad19

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {
+    name: `Diego`,
+    streetAddress: `1111 Main Ln`,
+}
+
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let employeeClone = {...employee};
+    delete employeeClone[key];
+    return employeeClone
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee
+}
